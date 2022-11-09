1704b76d7c8f9a6a22269832a8814f5de9ae88c9
gregor256 <gl@csgo.com>
2022-11-09 21:27:05 +0100

    7a

--- HW_7/hw_7_a.py
+++ HW_7/hw_7_a.py
@@ -1,5 +1,5 @@
-M = 1e9 + 7
-P = 199
+M = 2_038_074_743
+P = 1_299_709
 
 
 class StringHash:
@@ -10,20 +10,20 @@ class StringHash:
         if string:
             self.hashes[0] = self.my_ord(string[0])
             for i in range(1, len(string)):
-                self.hashes[i] = int(self.hashes[i - 1] * P + self.my_ord(string[i]) % M)
+                self.hashes[i] = int((self.hashes[i - 1] * P + self.my_ord(string[i])) % M)
                 self.powers[i] = int((self.powers[i - 1] * P) % M)
 
     @staticmethod
     def my_ord(letter):
-        return ord(letter) + 10
+        return ord(letter) + 42
 
     def get_hash(self, left, right):
         if not self.string:
             return 0
         if left == 0:
             return self.hashes[right]
-        return int((self.hashes[right] - ((self.hashes[left] *
-                                           self.powers[right - left]) % M) + M) % M)
+        return int((self.hashes[right] - self.hashes[left - 1] *
+                    self.powers[right - left + 1]) % M)
 
     def are_substrings_equal(self, left_1, right_1, left_2, right_2):
         if right_1 - left_1 != right_2 - left_2:
@@ -31,32 +31,18 @@ class StringHash:
         return self.get_hash(left_1 - 1, right_1 - 1) == self.get_hash(left_2 - 1, right_2 - 1)
 
 
-def solve(std_input=True, s='', poses=None):
-    if std_input:
-        s = input()
-        iterations = int(input())
-    else:
-        iterations = 1
+def solve():
+    s = input()
+    iterations = int(input())
     hash_string = StringHash(s)
     for iteration in range(iterations):
-        if std_input:
-            task_left_1, task_right_1, task_left_2, task_right_2 = tuple(map(int, input().split()))
-        else:
-            task_left_1, task_right_1, task_left_2, task_right_2 = poses
-        if hash_string.are_substrings_equal(task_left_1, task_right_1, task_left_2, task_right_2):
-            # pass
+        task_left_1, task_right_1, task_left_2, task_right_2 = tuple(map(int, input().split()))
+        result = hash_string.are_substrings_equal(task_left_1, task_right_1, task_left_2, task_right_2)
+        if result:
             print('Yes')
         else:
-            # pass
             print('No')
 
 
 if __name__ == '__main__':
-    # ss = 'trololo'
-    # for a in range(1, len(ss) + 1):
-    #     for b in range(a, len(ss) + 1):
-    #         for c in range(1, len(ss) + 1):
-    #             for d in range(c, len(ss) + 1):
-    #                 print(a, b, c, d, end=' --- ')
-    #                 solve(False, ss, (a, b, c, d))
     solve()
